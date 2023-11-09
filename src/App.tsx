d9ab2cca966de507495ec49fbd7fa48ccfec3ff3
Sumant-Dusane <sumant.dusane1966@gmail.com>
2023-11-09 20:42:40 +0530

    added more ui functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import React, { useState } from 'react';
-import { Box, Card, CardContent, Typography, Button, TextField } from '@mui/material';
+import { Box, Card, CardContent, Typography, Button, TextField, styled, InputAdornment, IconButton, Link, Divider } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-import { FacebookOutlined } from '@mui/icons-material';
+import { CloseRounded, FacebookOutlined, Image } from '@mui/icons-material';
 
 export default function Landing() {
   const [isLoading, setLoadingStatus] = useState(false);
   const [showUrl, setShowUrl] = useState(false);
   const [showPostFragment, setShowPostFragment] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<any>();
+  const [url, setUrl] = useState<string>('');
+
+  const connectToFB = () => {
+    setLoadingStatus(true)
+
+    setTimeout(() => {
+      setLoadingStatus(false)
+    }, 5000)
+  }
+
+  const setCurrentImageCustom = (value: any) => {
+    setSelectedImage(value)
+  }
+
+  const setCurrentImage = (e: any) => {
+    setSelectedImage(URL.createObjectURL(e?.target?.files[0]));
+  }
+
+  const selectImageUsingURL = (e: any) => {
+    if (e.key === 'Enter') {
+      setSelectedImage(e.target.value);
+    }
+  }
+
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
+
   return (
     <Box textAlign='center' marginTop={2}>
       <img
@@ -33,41 +71,60 @@ export default function Landing() {
             loading={isLoading}
             loadingPosition="center"
             variant="contained"
-            sx={{width: '100%', borderRadius: '1rem', padding: '.5rem', fontSize: '1rem', '@media(min-width: 769px)': {width: '50%'}}}
+            sx={{ width: '100%', borderRadius: '1rem', padding: '.5rem', fontSize: '1rem', '@media(min-width: 769px)': { width: '50%' } }}
+            onClick={connectToFB}
           >
             <span>Connect to Facebook</span>
           </LoadingButton>
 
-          <Button sx={{ width: '100%', fontSize: '1rem', '@media(min-width: 769px)': {width: '50%'}}} variant='outlined' onClick={() => setShowPostFragment(!showPostFragment)}>POST</Button>
+          <Button sx={{ width: '100%', fontSize: '1rem', '@media(min-width: 769px)': { width: '50%' } }} variant='outlined' disabled={isLoading} onClick={() => setShowPostFragment(!showPostFragment)}>POST</Button>
         </CardContent>
       </Card>
 
       <Card variant='outlined' sx={{
         mt: 2,
         overflow: 'hidden',
-        maxHeight: showPostFragment ? 'max-content' : 0,
-        opacity: showPostFragment ? '1' : 0,
+        maxHeight: showPostFragment && !isLoading ? 'max-content' : 0,
+        opacity: showPostFragment && !isLoading ? '1' : 0,
         transition: '400ms all ease-in-out'
       }}
         component='fieldset'>
         <legend style={{ textAlign: 'start', color: 'GrayText' }}>Post Image</legend>
         <CardContent>
           <Typography variant='h6' fontWeight='semibold' marginY={2} >Select an Image & Post it to your Facebook</Typography>
-          <Box sx={{ width: '100%', margin: '0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 2, '@media(min-width: 769px)': {width: '50%'}}}>
-            <Button variant='contained' style={{ borderRadius: '100px', padding: '.5rem 2rem', fontSize: '1rem' }}>Upload Image</Button>
+          <Box sx={{ width: '100%', margin: '0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 2, '@media(min-width: 769px)': { width: '50%' } }}>
+            <Button component="label" variant='contained' startIcon={<Image />} style={{ borderRadius: '100px', padding: '.5rem 2rem', fontSize: '1rem' }}>
+              Upload Image
+              <VisuallyHiddenInput type="file" onChange={setCurrentImage} />
+            </Button>
             <Typography variant='h6'>or drop a file,</Typography>
             <Typography variant="body2" color="text.secondary" align="center">
               {'Paste Image or '}
               <Button variant='text' sx={{ padding: '0 5px', textDecoration: 'underline !important', minWidth: 'max-content' }} onClick={() => setShowUrl(!showUrl)}>URL</Button>
             </Typography>
-            <Box sx={{ height: '1.4375em', width: '100%' }}>
-              {showUrl && <TextField id="outlined-basic" label="URL" variant="outlined" fullWidth sx={{ height: '100%' }} />}
+            <Box sx={{ height: '56px', width: '100%' }}>
+              {showUrl &&
+                <TextField
+                  fullWidth
+                  id="outlined-basic"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  label="URL"
+                  variant="outlined"
+                  sx={{ height: '100%', 'input:-internal-autofill-selected': { boxShadow: '0 0 0 30px #FFFFFF inset' } }}
+                  onKeyDown={selectImageUsingURL}
+                  InputProps={{
+                    endAdornment:
+                      <InputAdornment position='end'>
+                        <IconButton><CloseRounded onClick={() => setUrl('')} ></CloseRounded></IconButton>
+                      </InputAdornment>
+                  }}
+                />}
             </Box>
           </Box>
-
-          <Box mt={12} mb={2} marginX={'auto'} width='50%'>
+          <Box marginX={'auto'} width='50%'>
             <Typography variant='body1' fontWeight='bold' marginY={2} color={'GrayText'}>No Image? Try one of these:</Typography>
-            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', alignItems: 'center', justifyItems: 'center', gap: '1rem', '@media(min-width: 769px)': {gridTemplateColumns: 'repeat(5, 1fr)'}}}>
+            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', alignItems: 'center', justifyItems: 'center', gap: '1rem', '@media(min-width: 769px)': { gridTemplateColumns: 'repeat(5, 1fr)' } }}>
               {[...Array(5)].map((e: any, i: any) => (
                 <Box key={i} sx={{
                   width: '100%',
@@ -81,10 +138,26 @@ export default function Landing() {
                     src="https://random.imagecdn.app/150/150"
                     alt="Random Image"
                     loading="lazy"
+                    onClick={() => setCurrentImageCustom('https://random.imagecdn.app/150/150')}
                   />
                 </Box>
               ))}
             </Box>
+          </Box>
+          <Divider sx={{margin: '4rem 0'}} />
+          <Box 
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            {selectedImage &&
+              <>
+                <img src={selectedImage} style={{ maxHeight: '315px', width: 'auto', objectFit: 'contain', marginBottom: '1rem' }} />
+                <Button sx={{ textDecoration: 'underline !important' }} variant='text' onClick={() => setCurrentImageCustom(null)}>Remove selected Image</Button>
+              </>
+            }
           </Box>
         </CardContent>
       </Card>
